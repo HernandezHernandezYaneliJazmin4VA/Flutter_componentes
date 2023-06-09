43fd0c482c5afe747474a08f5c49e8d0f6178bc4
YANELI_JAZMIN_HERNANDEZ_HERNANDEZ <a.20308051281007@cbtis128.edu.mx>
2023-06-09 21:51:04 +0000

    WAAAA

--- lib/screens/textbutton_screen.dart
+++ lib/screens/textbutton_screen.dart
@@ -25,122 +25,209 @@ class _textbuttonScreenState extends State<textbuttonScreen> {
           children: <Widget>[
             ListView(
               physics: const AlwaysScrollableScrollPhysics(),
-              padding: const EdgeInsets.all(30),
               children: <Widget>[
-                TextButton(
-                  child: Text("Button"),
-                  onPressed: () {},
-                ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.pink,
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("Button"),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
                 ),
-                TextButton(
-                  child: Text("Button"),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Color(0xffe07a9d),
-                    onSurface: Colors.black,
-                  ),
-                  onPressed: null,
-                ),
-                TextButton(
-                  child: Text("button"),
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.pink),
-                    overlayColor:
-                        MaterialStateProperty.all<Color>(Color(0xff66142f)),
-                  ),
-                  onPressed: () {},
-                ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
                       primary: Colors.white,
                       backgroundColor: Colors.pink,
-                      side: BorderSide(color: Color(0xff66142f), width: 1)),
-                  onPressed: () {},
-                ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.pink,
-                    side: BorderSide(color: Color(0xff66142f), width: 1),
-                    elevation: 20,
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
                 ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.pink,
-                    side: BorderSide(color: Color(0xff66142f), width: 1),
-                    elevation: 20,
-                    minimumSize: Size(100, 50),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("Button"),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Color(0xffe07a9d),
+                      onSurface: Colors.black,
+                    ),
+                    onPressed: null,
                   ),
-                  onPressed: () {},
                 ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.pink,
-                    side: BorderSide(color: Color(0xff66142f), width: 1),
-                    elevation: 20,
-                    minimumSize: Size(100, 50),
-                    shadowColor: Colors.red,
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.pink),
+                      overlayColor:
+                          MaterialStateProperty.all<Color>(Color(0xff66142f)),
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
                 ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.pink,
-                    side: BorderSide(color: Color(0xff66142f), width: 1),
-                    elevation: 20,
-                    minimumSize: Size(100, 50),
-                    shadowColor: Colors.red,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.pink,
+                        side: BorderSide(color: Color(0xff66142f), width: 1)),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
                 ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.pink,
-                    side: BorderSide(color: Color(0xff66142f), width: 1),
-                    elevation: 30,
-                    minimumSize: Size(100, 70),
-                    shadowColor: Colors.red,
-                    shape: CircleBorder(),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.pink,
+                      side: BorderSide(color: Color(0xff66142f), width: 1),
+                      elevation: 20,
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
                 ),
-                TextButton(
-                  child: Text("button"),
-                  style: TextButton.styleFrom(
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.pink,
+                      side: BorderSide(color: Color(0xff66142f), width: 1),
+                      elevation: 20,
+                      minimumSize: Size(100, 50),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
                       primary: Colors.white,
                       backgroundColor: Colors.pink,
                       side: BorderSide(color: Color(0xff66142f), width: 1),
                       elevation: 20,
                       minimumSize: Size(100, 50),
                       shadowColor: Colors.red,
-                      padding: EdgeInsets.only(
-                          left: 60, right: 60, top: 15, bottom: 15)),
-                  onPressed: () {},
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.pink,
+                      side: BorderSide(color: Color(0xff66142f), width: 1),
+                      elevation: 20,
+                      minimumSize: Size(100, 50),
+                      shadowColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Colors.pink,
+                      side: BorderSide(color: Color(0xff66142f), width: 1),
+                      elevation: 30,
+                      minimumSize: Size(100, 70),
+                      shadowColor: Colors.red,
+                      shape: CircleBorder(),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.pink,
+                        side: BorderSide(color: Color(0xff66142f), width: 1),
+                        elevation: 20,
+                        minimumSize: Size(100, 50),
+                        shadowColor: Colors.red,
+                        padding: EdgeInsets.only(
+                            left: 60, right: 60, top: 15, bottom: 15)),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.pink,
+                        side: BorderSide(color: Color(0xff66142f), width: 1),
+                        elevation: 20,
+                        minimumSize: Size(100, 50),
+                        shadowColor: Colors.red,
+                        padding: EdgeInsets.all(30)),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: TextButton(
+                    child: Text("button"),
+                    style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.pink,
+                        side: BorderSide(color: Color(0xff66142f), width: 1),
+                        elevation: 20,
+                        minimumSize: Size(100, 50),
+                        shadowColor: Colors.red,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 50)),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.all(30),
+                    child: TextButton(
+                      child: Text("button"),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.pink),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          elevation: MaterialStateProperty.all<double>(20),
+                          minimumSize:
+                              MaterialStateProperty.all<Size>(Size(100, 50)),
+                          shadowColor:
+                              MaterialStateProperty.all<Color>(Colors.red),
+                          shape: MaterialStateProperty.all<CircleBorder>(
+                              CircleBorder()),
+                          padding:
+                              MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                  EdgeInsets.all(30)),
+                          textStyle: MaterialStateProperty.all<TextStyle>(
+                              TextStyle(
+                                  fontSize: 20, fontStyle: FontStyle.italic))),
+                      onPressed: () {},
+                    )),
               ],
             ),
             Center(
